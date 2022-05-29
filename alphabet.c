d8068a3da846e16bce94b8a96fe4b80075fec6b4
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-05-29 20:03:41 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,27 @@
+// #include<stdio.h>
+// int main()
+// {
+//     char x;
+//     for ( x = 'a'; x <= 'z'; x++)
+//     {
+//         printf("%c ",x);
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+// a value 
+#include<stdio.h>
+int main()
+{
+    char x;
+    for ( x = 'a'; x <= 'z'; x++)
+    {
+        printf("%d ",x);
+    }
+    
+    return 0;
+}

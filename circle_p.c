da13098886cb01b68ac55e4b86630cc756959385
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-05-29 20:06:20 +0530

    Add files via upload

---
+++ circle_p.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius, diameter, circumference, area;
+    
+    /*
+     * Input radius of circle from user
+     */
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+
+    /*
+     * Calculate diameter, circumference and area
+     */
+    diameter = 2 * radius;
+    circumference = 2 * 3.14 * radius;
+    area = 3.14 * (radius * radius);
+
+    /*
+     * Print all results
+     */
+    printf("Diameter of circle = %.2f units \n", diameter);
+    printf("Circumference of circle = %.2f units \n", circumference);
+    printf("Area of circle = %.2f sq. units ", area);
+
+    return 0;
+}

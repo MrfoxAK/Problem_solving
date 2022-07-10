c792c9352b1d94906eb49ece36580b17f61a2be5
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-07-10 12:03:17 +0530

    Add files via upload

---
+++ problem_on_if-else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+     int x=5;
+     if (x==4)
+     printf("right");
+     printf("%d",x); //if i write this line its give a error
+     else
+     printf("wrong");
+     printf("%d",x);
+     return 0;
+}

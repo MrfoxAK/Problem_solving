4c279bfdb3cc1ab443b2fe09f9cfd925b13951dd
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-04-12 10:26:09 +0530

    Add files via upload

--- star_pattern_57.c
+++ star_pattern_57.c
@@ -3,22 +3,48 @@ int main()
 {
      int n;
      printf("Enter the Row: ");
-     scanf("%d",&n);
-     int k=1,f=0;
-     for (int i = 0; i < n; i++)
+     scanf("%d", &n);
+     int k = 1, f = 0;
+     for (int i = 0; i < n+1; i++)
      {
-          k=1;
-          f=0;
-          for (int j = 0; j < i*2+1; j++)
+          k = 1;
+          f = 0;
+          for (int j = 0; j < i * 2 + 1; j++)
           {
-               if(k<i+1 && f==0){
-                    printf("%d",k);
+               if (i * 2 + 1 == j + 1 || j == 0)
+               {
+                    printf("*");
+               }
+               else if (j < i)
+               {
+                    printf("%d", k);
                     k++;
                }
-               else{
-                    f=1;
-                    printf("%d",k);
+               else
+               {
+                    printf("%d", k);
                     k--;
+               }
+          }
+          printf("\n");
+     }
+     for (int i = 0; i < n; i++)
+     {
+          k = 1;
+          for (int j = 0; j < n-i-1; j++)
+          {
+               if ( j == 0)
+               {
+                    printf("*");
+               }
+               printf("%d",k);
+               k++;
+          }
+          for (int j = n-i-2; j > 0; j--)
+          {
+               printf("%d",j);
+               if(j-1==0){
+                    printf("*");
                }
           }
           printf("\n");

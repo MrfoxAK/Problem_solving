e2d3c3594aae7a778d8f14791dc02c74f2b5fe2a
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-04-18 19:43:12 +0530

    Add files via upload

---
+++ tricky_qusn_part1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+     int i = 0;
+     for(;i<=5;i++);
+          printf("%d",i);
+     return 0;
+}
+// as of the semicolon
+
+
---
+++ tricky_qusn_part1_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+     unsigned int i = 65535;
+     while (i++!=0)
+          printf("%d",i++);
+     printf("\n");
+     return 0;
+}
